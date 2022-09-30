4d89b65724a03dd519492fa3d592f19e8fb13f05
victormgalvanc <72720374+VictorG222@users.noreply.github.com>
2022-09-29 18:29:58 -0700

    Login y registro

--- App193/app/src/main/java/com/victor/app193/Authentication/Login.java
+++ App193/app/src/main/java/com/victor/app193/Authentication/Login.java
@@ -2,13 +2,45 @@ package com.victor.app193.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.victor.app193.R;
 
 public class Login extends AppCompatActivity {
+
+    Button login, register;
+    EditText textoCorreo, textoPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.btnLogin);
+        register = findViewById(R.id.btnRegistro);
+        textoCorreo = findViewById(R.id.correo);
+        textoPassword = findViewById(R.id.password);
+
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {metodoExplicito(); }
+//        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                metodoExplicito();
+            }
+        });
+    }
+
+    private void metodoExplicito(){
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
+        finish();
     }
 }
--- App193/app/src/main/java/com/victor/app193/Authentication/Register.java
+++ App193/app/src/main/java/com/victor/app193/Authentication/Register.java
@@ -2,13 +2,46 @@ package com.victor.app193.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.victor.app193.R;
 
 public class Register extends AppCompatActivity {
+
+    Button login, register;
+    EditText textoCorreo, textoPassword, textoPasswordConfirmar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        login = findViewById(R.id.btnLoginRegistro);
+        register = findViewById(R.id.btnRegistroRegistro);
+        textoCorreo = findViewById(R.id.correoRegistro);
+        textoPassword = findViewById(R.id.contraseniaRegistro);
+        textoPasswordConfirmar = findViewById(R.id.contraseniaConfirmarRegistro);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {metodoExplicito(); }
+        });
+
+//        register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                metodoExplicito();
+//            }
+//        });
+    }
+
+    private void metodoExplicito(){
+        Intent intent = new Intent(Register.this, Login.class);
+        startActivity(intent);
+        finish();
     }
 }
